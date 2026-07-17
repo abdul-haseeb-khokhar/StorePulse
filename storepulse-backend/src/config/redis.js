7815eca386620bef34e40fcfed55b41295839c5d
abdul-haseeb-khokhar <h.khokhar2015@gmail.com>
2026-07-16 21:44:46 -0700

    Added redis retry strategy

--- storepulse-backend/src/config/redis.js
+++ storepulse-backend/src/config/redis.js
@@ -1,16 +1,35 @@
 const Redis = require('ioredis');
 
-const redisClient = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || 6379
-});
+const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+
+const redisClient = new Redis(redisUrl,{
+    retryStrategy(times) {
+        const delay = Math.min(times * 500, 10000);
+        console.warn(`Redis reconnect attempt ${times}, retrying in ${delay}ms`);
+        return delay;
+    },
+    maxRetriesPerRequest: 3,
+    enableOfflineQueue: true,
+})
 
 redisClient.on('connect', () => {
     console.log('Redis Connected');
-})
+});
+
+redisClient.on('ready', () => {
+    console.log('Redis Ready');
+});
 
 redisClient.on('error', (err) => {
     console.error("Redis Error: ", err)
+});
+
+redisClient.on('reconnecting', (delay) => {
+    console.log(`Redis reconnecting in ${delay}ms`);
+});
+
+redisClient.on('close', ()=> {
+    console.warn('Redis connection closed');
 });
 
 module.exports = redisClient;
